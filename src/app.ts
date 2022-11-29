b1229ec786a18cb8630eccc2dba11a64af9562cb
Douglas Hellowell <doog99@gmail.com>
2022-11-29 09:41:02 -0800

    add req log in app.ts

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,12 @@ import { getStatsBySummonerName } from './controllers/lol.controller';
 
 const app = express();
 
+app.use((req, res, next) => {
+  console.log(`${req.method}: ${req.url}`);
+
+  next();
+});
+
 app.get('/', (req, res, next) => {
   res.send({ msg: 'hello there' });
 });
